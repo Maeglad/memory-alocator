004a52e287d689b0044f6c309c555fee759a61f8
Lubos Miklosovic <miklosovicl@gmail.com>
2015-04-11 02:05:00 +0200

    initial commit

---
+++ alloc.c
@@ -0,0 +1,95 @@
+#include "wrapper.h"
+#include <stdio.h>
+/* Kod funkcii my_alloc a my_free nahradte vlastnym. Nepouzivajte ziadne
+ * globalne ani staticke premenne; jedina globalna pamat je dostupna pomocou
+ * mread/mwrite/msize, ktorych popis najdete vo wrapper.h */
+
+/* Ukazkovy kod zvladne naraz iba jedinu alokaciu. V 0-tom bajte pamate si
+ * pamata, ci je pamat od 1 dalej volna alebo obsadena. 
+ *
+ * V pripade, ze je volna, volanie my_allloc skonci uspesne a vrati zaciatok
+ * alokovanej RAM; my_free pri volnej mamati zlyha.
+ *
+ * Ak uz nejaka alokacia prebehla a v 0-tom bajte je nenulova hodnota. Nie je
+ * mozne spravit dalsiu alokaciu, takze my_alloc musi zlyhat. my_free naopak
+ * zbehnut moze a uvolni pamat.
+ */
+
+
+/**
+ * Inicializacia pamate
+ *
+ * Zavola sa, v stave, ked sa zacina s prazdnou pamatou, ktora je inicializovana
+ * na 0.
+ */
+void my_init(void) {
+    mwrite(0, 0);
+    int memSize = msize()-3; 
+    uint8_t pom = memSize>>8; 
+    mwrite(1,pom);
+    mwrite(2,(uint8_t)(memSize & 255));  
+}
+
+/**
+ * Poziadavka na alokaciu 'size' pamate. 
+ *
+ * Ak sa pamat podari alokovat, navratova hodnota je adresou prveho bajtu
+ * alokovaneho priestoru v RAM. Pokial pamat uz nie je mozne alokovat, funkcia
+ * vracia FAIL.
+ *
+ * moja idea: hlavcka bude ulozena takto
+ *  1 bajt: 0(free) / 1(occupied)
+ *  2 bajty: velkost zapisana binarne a splitnuta na byty
+ *
+ *
+ * moje predpoklady su ze program testovaca nerobi writy do nepridelenej pamete
+ * pamet nepresiahne 2^16 lebo moje hlavicky su ukladane fixne na 3 byty a nechcem riskovat s malom casu
+ */
+int my_alloc(unsigned int size) {
+    int poloha = 0; 
+    // ak je mnozstvo pamete rovne 0 tak si osobne myslim ze allocovat nic nemusim
+    if(size == 0)return FAIL;
+    // hlavcku si musim ulozit nech sa deje cokolvek
+    if(size + 3 > msize())return FAIL;
+    // ak je poloha + size < msize tak ma zmysel dalej hladat
+    // pozriem sa na polohu ak je free pozbieram data o jej velkosti
+    // ak nie skocim na dalsiu
+    // ak najdem dost velku polohu tak ju skratim na moju velkost a vyrobim novy 
+    while( (poloha + size + 3) <= msize() ){
+        uint8_t obsadene;
+        int block = 0;
+        obsadene = mread(poloha);
+        block = mread(poloha+1)<<8;
+        block += mread(poloha+2);
+        if(obsadene == 1){
+        
+        }
+    }
+
+    return FAIL;
+}
+
+/**
+ * Poziadavka na uvolnenie alokovanej pamate na adrese 'addr'.
+ *
+ * Ak bola pamat zacinajuca na adrese 'addr' alokovana, my_free ju uvolni a
+ * vrati OK. Ak je adresa 'addr' chybna (nezacina na nej ziadna alokovana
+ * pamat), my_free vracia FAIL.
+ */
+
+int my_free(unsigned int addr) {
+
+	/* Adresa nie je platnym smernikom, ktory mohol vratit my_alloc */
+	if (addr != 1)
+		return FAIL;
+
+	/* Nie je alokovana ziadna pamat, nemozeme ju teda uvolnit */
+	if (mread(0) != 1)
+		return FAIL;
+
+	/* Vsetko je OK, mozeme uvolnit pamat */
+	mwrite(0, 0);
+	return OK;
+}
+
+
---
+++ wrapper.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+int MEMORY_SIZE = 4096;
+static FILE *mem = NULL;
+
+int minit(void) {
+	assert(mem == NULL);
+
+	mem = fopen("memory.bin", "rb+");
+	if (mem == NULL) {
+		/* Set up size of the file, if it does not exist */
+		unsigned char c=0;
+		int i;
+		mem = fopen("memory.bin", "w+");
+		fseek(mem, 0, SEEK_SET);
+		for (i = 0; i < MEMORY_SIZE; i++)
+			fwrite(&c, 1, 1, mem);
+
+		my_init();
+	} else {
+		fseek(mem, 0 , SEEK_END);
+		int file_size = ftell(mem);
+		assert(file_size == MEMORY_SIZE);
+	}
+
+	if (!mem) {
+		perror("fopen");
+		return -1;
+	}
+	
+	return 0;
+}
+
+int mdone(void) {
+	assert(mem != NULL);
+
+	fclose(mem);
+	mem = NULL;
+}
+
+uint8_t mread(unsigned int addr) {
+	uint8_t data = 0;
+	assert(mem != NULL);
+	assert((0 <= addr) && (addr < MEMORY_SIZE));
+
+	assert(fseek(mem, addr, SEEK_SET) == 0);
+	assert(fread(&data, 1, 1, mem) == 1);
+
+	return data;
+}
+
+void mwrite(unsigned int addr, uint8_t val) {
+	uint8_t data = 0;
+	assert(mem != NULL);
+	assert((0 <= addr) && (addr < MEMORY_SIZE));
+
+	assert(fseek(mem, addr, SEEK_SET) == 0);
+	assert(fwrite(&val, 1, 1, mem) == 1);
+	assert(fflush(mem) == 0);
+}
+
+unsigned int msize(void) {
+	return MEMORY_SIZE;
+}
+
+#define MAX_LINE_LENGTH 256
+
+int main(int argc, char *argv[]) {
+	char line[MAX_LINE_LENGTH], command[16];
+	int data, data2;
+	int params;
+
+	if (argc >1)
+		MEMORY_SIZE = atoi(argv[1]);
+	else
+		MEMORY_SIZE = 4096;
+
+	assert (MEMORY_SIZE > 0);
+
+	minit();
+
+	while (!feof(stdin)) {
+		fgets(line, MAX_LINE_LENGTH, stdin);
+		params = sscanf(line, "%15s %d %d\n", &command, &data, &data2);
+
+		if (0) {}
+		else if (!strcmp(command, "alloc") && params == 2)
+			fprintf(stdout, "%d\n", my_alloc(data)); 
+		else if (!strcmp(command, "free") && params == 2)
+			fprintf(stdout, "%d\n", my_free(data));
+		else if (!strcmp(command, "read") && params == 2)
+			fprintf(stdout, "%d\n", mread(data));
+		else if (!strcmp(command, "write") && params == 3) {
+			mwrite(data, data2);
+			fprintf(stdout, "%d\n", 0);
+		} else if (!strcmp(command, "end") && params == 1)
+			break;
+		else
+			fprintf(stderr, "Unknown command line '%s'\n", line);
+	}
+
+	mdone();
+
+	return 0;
+}
+
+
